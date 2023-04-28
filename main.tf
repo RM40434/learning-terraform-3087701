486358ada166c6b2c1cdf99194dbd27283217164
RM40434 <56568546+RM40434@users.noreply.github.com>
2023-04-28 14:27:44 -0500

    commented SG

--- main.tf
+++ main.tf
@@ -39,14 +39,16 @@ resource "aws_instance" "blog" {
   }
 }
 
+/*
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.vpc.dev.id
+  vpc_id  = module.vpc.
   name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
+*/
